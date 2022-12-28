8c90eb26d768f6021e5ea8672c132e4475670097
arphe42 <arphe42@gmail.com>
2022-12-28 14:14:36 -0500

    aaaaa

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,26 @@ fn hahaha() -> String {
     return String::from("Hello World!");
 }
 
-fn main() {
-    let x = String::from("hello world!");
+fn two_times(x:i32) -> i32 {
+    return x * 2;
+}
 
-    println!("{}", x);
+fn comparaison(x:i32) -> bool {
+    return x == 2;
+}
+
+fn main() {
     println!("{}", hahaha());
+
+    let mut x: i32 = 0;
+    while x <= 20 {
+        println!("{}", two_times(x));
+        x += 1;
+    }
+
+    for n in 1..=15 {
+        println!("{}", n);
+    }
+
+    println!("{}", comparaison(2));
 }
